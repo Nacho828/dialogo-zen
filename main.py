98afdb40fdef1e37b6aaac474269151a6d21442c
Nacho828 <nchomaxanton@gmail.com>
2025-03-25 23:15:39 +0100

    si 2

---
+++ main.py
@@ -0,0 +1,11 @@
+from lanzador import main as lanzador_main
+
+def main():
+    """
+    Punto de entrada principal del programa.
+    """
+    print("Bienvenido a Diálogo Zen")
+    # Aquí puedes agregar la lógica principal de tu programa.
+
+if __name__ == "__main__":
+    lanzador_main()
